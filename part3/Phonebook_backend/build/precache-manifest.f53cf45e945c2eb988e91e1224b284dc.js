457081476f0262875e7a2eb08c982d82f8187ff0
lostyu <lostyu789@163.com>
2020-05-19 08:54:45 +0800

    part3 v3.9

---
+++ part3/Phonebook_backend/build/precache-manifest.f53cf45e945c2eb988e91e1224b284dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c99d69b381e5a052ddba0eb4af2f6c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e6f531ea99a0ab093b1",
+    "url": "/static/css/main.f8872d8b.chunk.css"
+  },
+  {
+    "revision": "37c0128e2b692b45254f",
+    "url": "/static/js/2.0377991e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0377991e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6f531ea99a0ab093b1",
+    "url": "/static/js/main.72c04864.chunk.js"
+  },
+  {
+    "revision": "09d1d8cf6d988010c0d9",
+    "url": "/static/js/runtime-main.d2fe6083.js"
+  }
+]);
--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('type', (req, res) => {
   if (req.method === 'POST') {
@@ -11,6 +12,8 @@ morgan.token('type', (req, res) => {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+app.use(cors())
+app.use(express.static('build'))
 
 
 let persons = [
