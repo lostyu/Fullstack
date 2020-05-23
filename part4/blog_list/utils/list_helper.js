5a240f1a62d6d537ad6bc892789efce599e52526
lostyu <lostyu789@163.com>
2020-05-23 10:34:09 +0800

    part4 v4.4 test

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, obj) => {
+    return sum + obj.likes
+  }
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
