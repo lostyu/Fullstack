a49ed61272b73b55c529c022fb172359d789c448
lostyu <lostyu789@163.com>
2020-05-23 11:07:49 +0800

    part4 v4.5 test

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -12,7 +12,24 @@ const totalLikes = (blogs) => {
     : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let max = 0
+  let _index = 0
+
+  blogs.forEach((item, index) => {
+    if (max < item.likes) {
+      max = item.likes
+      _index = index
+    }
+  })
+
+  return blogs.length === 0
+    ? null
+    : blogs[_index]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
