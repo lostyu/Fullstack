3a1766483657f7160962bb6d92a0acd28556ce6c
lostyu <lostyu789@163.com>
2020-05-25 16:01:05 +0800

    part4 v4.15 v4.16

---
+++ part4/blog_list/controllers/users.js
@@ -0,0 +1,29 @@
+const usersRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+
+usersRouter.post('/', async (req, res) => {
+  const body = req.body
+
+  if (body.password.length < 3) {
+    return res.status(400).json({ error: 'password length is min 3' })
+  }
+
+  const saltRound = 10
+
+  const passwordHash = await bcrypt.hash(body.password, saltRound)
+
+  const newUser = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash
+  })
+
+  const savedUser = await newUser.save()
+
+  res.json(savedUser.toJSON())
+
+})
+
+module.exports = usersRouter
