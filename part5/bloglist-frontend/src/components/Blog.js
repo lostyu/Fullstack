8693f7f0915fc7414ffb8e388eb741964c8d4a52
lostyu <lostyu789@163.com>
2020-05-28 12:45:05 +0800

    part5 v5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const show = { display: visible ? '' : 'none' }
+
+  const toggleVisible = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div>
+      <div style={blogStyle}>
+        <p>{blog.title} <button onClick={toggleVisible}>{visible ? 'hide' : 'view'}</button></p>
+        <div style={show}>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.author}</p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
 
 export default Blog
