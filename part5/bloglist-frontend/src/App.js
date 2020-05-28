87312244d14a95733d47966deaf6fc5c4ee912b0
lostyu <lostyu789@163.com>
2020-05-28 15:10:12 +0800

    part5 v5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -142,7 +142,7 @@ const App = () => {
       <h2>create new</h2>
       {newBlogForm()}
 
-      {blogs.map(blog =>
+      {blogs.sort((a,b)=>b.likes-a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} addLike={addLike} />
       )}
     </div>
